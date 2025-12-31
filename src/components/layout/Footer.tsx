a208027d55659d3cb4da568d3ac4cc9415326570
chatgptnotes <chatgptnotes@gmail.com>
2025-12-31 23:00:35 +0530

    chore: Bump version to 2.5

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -91,7 +91,7 @@ export function Footer() {
             </div>
           </div>
           <p className="text-xs text-gray-600 mt-4 text-center">
-            Version 2.4 | Last Updated: December 31, 2024 | Repository: chatgptnotes/medsbharat.com
+            Version 2.5 | Last Updated: December 31, 2024 | Repository: chatgptnotes/medsbharat.com
           </p>
         </div>
       </div>
